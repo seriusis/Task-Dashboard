f154775a661108439fa7566aa5f4e1aae6532531
Seriusis <seriusisu@gmail.com>
2022-05-05 14:44:59 +0300

    add router

---
+++ router.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+//$route = $_SERVER['REQUEST_URI'];
+//$action = 'index';
+
+//switch ($route){
+//    case '/' : $controller = new app\controller\HomeController();
+//    break;
+//
+//    case '/form' : $controller = new app\controller\FormController();
+//        break;
+//}
+//$controller->$action();
+
+
+$router = new \Bramus\Router\Router();
+
+$router->setNamespace('app\controller');
+
+$router->get('/', 'HomeController@index');
+
+$router->get('/form', 'FormController@index');
+$router->post('/success', 'FormController@success');
+
+$router->get('/list', 'TaskController@list');
+$router->get('/article/{id}', 'TaskController@task');
+$router->get('/create', 'TaskController@create');
+$router->post('/create', 'TaskController@create');
+
+$router->get('/update/{id}', 'TaskController@update');
+$router->post('/update/{id}', 'TaskController@update');
+
+
+$router->run();
