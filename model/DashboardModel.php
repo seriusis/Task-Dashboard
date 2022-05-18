ebd848c053fd060594967f57bb6fe90948b25098
Seriusis <seriusisu@gmail.com>
2022-05-18 11:30:52 +0300

    decorate dashboard list view, count dashboard tasks (dashboard model)

--- model/DashboardModel.php
+++ model/DashboardModel.php
@@ -7,14 +7,16 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class DashboardModel extends BaseModel{
     public function __construct()
     {
-        parent::__construct('dashboard');
+        parent::__construct('dashboard as d');
     }
 
 
     public function getUserDashboards(){
         return Capsule::table($this->table)
-            ->select(['id', 'name', 'date'])
-            ->where('user_id','=',$_SESSION['user_id'])
+            ->selectRaw('d.id, d.name, d.date, COUNT(t.id) as total')
+            ->leftJoin('task as t','d.id','=','t.dashboard_id')
+            ->where('d.user_id','=',$_SESSION['user_id'])
+            ->groupBy('d.id')
             ->get()->all();
     }
 }
